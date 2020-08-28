2ada4df605c4da1be42d56f7433001ddb9957199
Francisco Michael <michael.soarez.07@gmail.com>
2020-08-28 18:29:15 -0300

    Paginas Dinamicas

--- perfis/views.py
+++ perfis/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
 
-# Create your views here.
+def exibir(request, perfil_id):
+    perfil = Perfil()
+    
+    if perfil_id == 1:
+        perfil = Perfil(1, 'Elvis','elvis@gmail.com', '99999-9999', 'IFPI')
+
+    if perfil_id == 2:
+        perfil = Perfil(2, 'Lucas', 'lucas@gmail.com', '98888-8888', 'UFPI')
+
+    return render(request, 'perfil.html', { "perfil" : perfil})
